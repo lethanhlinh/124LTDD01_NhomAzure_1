ad481043c0e4ec14efa90bd4c1020fb1dd6c56cf
Nguyễn Duy Việt <duyvietnguyen0805@gmail.com>
2024-12-21 07:44:51 +0700

    Tao Project

--- app/src/main/java/com/example/baitapnhomandroid/Activity/MainActivity.java
+++ app/src/main/java/com/example/baitapnhomandroid/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.baitapnhomandroid;
+package com.example.baitapnhomandroid.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.baitapnhomandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
